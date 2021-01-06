063859e724f76b592aa62898370edc23c966561c
omimhetras <76042568+omimhetras@users.noreply.github.com>
2021-01-06 15:05:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var a,b;
+function setup() {
+  createCanvas(800,400);
+  a = createSprite(400, 200, 50, 50);
+  b = createSprite(200, 100, 30, 30);
+  a.shapeColor = "blue";
+  b.shapeColor = "blue";
+
+}
+
+function draw() {
+  background("red");  
+  a.x = World.mouseX;
+  a.y = World.mouseY;
+  console.log(a.x-b.x);
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2){
+    a.shapeColor = "gold";
+    b.shapeColor = "gold";
+  }
+  else{
+    a.shapeColor = "blue";
+    b.shapeColor = "blue";
+  }
+  drawSprites();
+}
